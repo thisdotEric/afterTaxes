1773a9ad50a2c22b87a4ba7b51399d152fe06203
thisdotEric <siguenzajohneric@gmail.com>
2021-12-03 00:18:07 +0800

    Implemented login user interface design

--- web/src/components/Home/Home.tsx
+++ web/src/components/Home/Home.tsx
@@ -1,15 +1,18 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 import './Home.css';
 import { Login } from '../Login';
-import { DailyQuotes } from '../DailyQuotes';
+import github from '../../images/github.png';
 
 interface HomeProps {}
 
 const Home: FC<HomeProps> = ({}: HomeProps) => {
   return (
     <div className="home">
-      <DailyQuotes />
       <Login />
+
+      <a href="https://github.com/thisdotEric/afterTaxes" target="_blank">
+        <img src={github} alt="Github" width={30} height={30} />
+      </a>
     </div>
   );
 };
--- web/src/components/Login/Login.tsx
+++ web/src/components/Login/Login.tsx
@@ -6,7 +6,20 @@ interface LoginProps {}
 const Login: FC<LoginProps> = ({}: LoginProps) => {
   return (
     <div className="login">
-      <p>Login</p>
+      <p id="app-name">afterTaxes</p>
+      <div className="fields">
+        <form action="">
+          <input type="text" name="email" placeholder="Email" />
+          <input type="password" name="email" placeholder="Password" />
+
+          <div id="remember">
+            <input type="checkbox" name="remember-me" />
+            <p>Remember Me</p>
+          </div>
+
+          <input type="submit" name="login" value="Login" />
+        </form>
+      </div>
     </div>
   );
 };
