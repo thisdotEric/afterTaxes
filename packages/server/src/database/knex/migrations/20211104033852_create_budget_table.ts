d5df53f17bea371b1f16c49e73f9f924cda2d369
thisdotEric <siguenzajohneric@gmail.com>
2022-04-14 17:06:04 +0800

    Updated budget table migration file

--- packages/server/src/database/knex/migrations/20211104033852_create_budget_table.ts
+++ packages/server/src/database/knex/migrations/20211104033852_create_budget_table.ts
@@ -1,34 +1,19 @@
 import { Knex } from 'knex';
-import { DbNames, BudgetType } from '../../constants';
+import { DbNames } from '../../constants';
+import { BUDGET_TYPES } from '../../constants/budgets';
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema
-    .createTable(DbNames.BUDGET_NAME, (table: Knex.TableBuilder) => {
-      table.string('budget_name').primary().notNullable().unique();
-      table.string('description').nullable();
-    })
-
-    .createTable(DbNames.BUDGET, (table: Knex.TableBuilder) => {
-      table
-        .increments('budget_id')
-        .notNullable()
-        .unique()
-        .primary()
-        .defaultTo(0);
-      table.string('budget_name').notNullable();
-      table.float('budget').unsigned();
-      table.date('date').notNullable();
-      table.enum('budget_type', Object.values(BudgetType)).defaultTo('daily');
-
-      table
-        .foreign('budget_name')
-        .references('budget_name')
-        .inTable(DbNames.BUDGET_NAME);
-    });
+  return knex.schema.createTable(DbNames.BUDGET, (table: Knex.TableBuilder) => {
+    table.increments('budget_id').notNullable().unique().primary().defaultTo(0);
+    table.enum('budget_type', Object.values(BUDGET_TYPES)).defaultTo('daily');
+    table.string('budget_name').notNullable();
+    table.float('budget').unsigned();
+    table.date('date_budgeted').notNullable();
+    table.date('from').notNullable();
+    table.date('to').notNullable();
+  });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema
-    .dropTableIfExists(DbNames.BUDGET)
-    .dropTableIfExists(DbNames.BUDGET_NAME);
+  return knex.schema.dropTableIfExists(DbNames.BUDGET);
 }
