991f9b1fd31c44b8cc69873a166878c924f48d2f
thisdotEric <siguenzajohneric@gmail.com>
2022-06-05 13:44:18 +0800

    Created expenses table migration file

---
+++ packages/server/src/database/knex/migrations/20220605053432_create_expenses_table.ts
@@ -0,0 +1,37 @@
+import { CATEGORIZED_BUDGET, EXPENSES, USERS } from '../../constants';
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable(EXPENSES, table => {
+    table
+      .increments('expenses_id')
+      .notNullable()
+      .unsigned()
+      .unique()
+      .primary()
+      .defaultTo(0);
+
+    table.date('date').notNullable();
+    table.string('name').notNullable();
+    table.float('amount').notNullable().unsigned();
+    table.string('description').nullable();
+
+    table.integer('user_id').notNullable();
+    table
+      .foreign('user_id')
+      .references('user_id')
+      .inTable(USERS)
+      .onDelete('CASCADE');
+
+    table.integer('categorized_budget_id').notNullable();
+    table
+      .foreign('categorized_budget_id')
+      .references('categorized_budget_id')
+      .inTable(CATEGORIZED_BUDGET)
+      .onDelete('CASCADE');
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists(EXPENSES);
+}
