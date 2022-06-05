633e415a73193cbcc3d4bcfc4f7b8ec66e4333e2
thisdotEric <siguenzajohneric@gmail.com>
2022-06-05 17:37:05 +0800

    Setup commons package which will hold the business logics of the application

---
+++ packages/commons/src/business/budget/budget.test.ts
@@ -0,0 +1,5 @@
+describe('', () => {
+  it('should pass', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ packages/commons/src/business/budget/budget.ts
@@ -0,0 +1,5 @@
+export class BudgetComputationService {
+  public computeRemainingBalance() {
+    return true;
+  }
+}
