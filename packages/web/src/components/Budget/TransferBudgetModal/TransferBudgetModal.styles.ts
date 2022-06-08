9efe76a5d7ab2798a6c7aa12700ec6ccc77f6245
thisdotEric <siguenzajohneric@gmail.com>
2022-06-08 21:29:19 +0800

    Added checks if all the values are filled up before transfers of budget

--- packages/web/src/components/Budget/TransferBudgetModal/TransferBudgetModal.styles.ts
+++ packages/web/src/components/Budget/TransferBudgetModal/TransferBudgetModal.styles.ts
@@ -4,6 +4,7 @@ import { green } from '../../../components/styles/colors';
 export const ConfirmationTransfer = styled.p`
   color: gray;
   padding-bottom: 20px;
+  padding-top: 10px;
 
   span {
     color: ${green};
--- packages/web/src/components/Budget/TransferBudgetModal/TransferBudgetModal.tsx
+++ packages/web/src/components/Budget/TransferBudgetModal/TransferBudgetModal.tsx
@@ -1,6 +1,6 @@
 import SharedModal from '../../Modal';
 import { FormWrapper } from '../../../components/styles/FormWrapper.styles';
-import React, { FC, useState } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 import type { RequiredModalProps } from '../../Modal/SharedModal';
 import { NumberInput, SelectInput, TextInput } from '../../../components/Input';
 import { Button } from '../../../components/Button';
@@ -29,10 +29,42 @@ const TransferBudgetModal: FC<TransferBudgetModalProps> = ({
   const [destinationBudgetID, setDestinationBudgetID] = useState(0);
   const [label, setLabel] = useState('');
 
+  const [amountError, setAmountError] = useState('');
+  const [budgetTypeError, setBudgetTypeError] = useState('');
+
+  const allValuesFilled = () => {
+    let withoutError = true;
+
+    if (destinationBudgetID == 0) {
+      setBudgetTypeError('No destination budget selected.');
+      withoutError = false;
+    }
+
+    if (amount == 0) {
+      setAmountError('Transfer budget should not be zero.');
+      withoutError = false;
+    }
+
+    if (amount > remainingBudget) {
+      setAmountError(`Limit amount of ${remainingBudget.toFixed(2)} exceeded.`);
+      withoutError = false;
+    }
+
+    return withoutError;
+  };
+
+  const clearStates = () => {
+    setAmount(0);
+    setDestinationBudgetID(0);
+    setBudgetTypeError('');
+    setAmountError('');
+  };
+
   return (
     <SharedModal
       opened={opened}
       onClose={() => {
+        clearStates();
         setOpened(false);
       }}
       title='Transfer Budget'
@@ -42,14 +74,17 @@ const TransferBudgetModal: FC<TransferBudgetModalProps> = ({
           onSubmit={async (e) => {
             e.preventDefault();
 
-            await axios.patch('budgets/transfer', {
-              from: id,
-              to: destinationBudgetID,
-              amount,
-            });
+            if (allValuesFilled()) {
+              await axios.patch('budgets/transfer', {
+                from: id,
+                to: destinationBudgetID,
+                amount,
+              });
 
-            setOpened(false);
-            await onSubmit();
+              clearStates();
+              setOpened(false);
+              await onSubmit();
+            }
           }}
         >
           <TextInput value={name} disabled label='From' />
@@ -62,11 +97,13 @@ const TransferBudgetModal: FC<TransferBudgetModalProps> = ({
           <SelectInput
             label='Transfer to'
             data={destinationBudgets}
+            error={budgetTypeError}
             onChange={(value) => {
               setDestinationBudgetID(parseInt(value!));
               setLabel(
                 destinationBudgets.filter((b) => b.value === value)[0].label
               );
+              setBudgetTypeError('');
             }}
           />
 
@@ -75,15 +112,23 @@ const TransferBudgetModal: FC<TransferBudgetModalProps> = ({
             value={amount}
             onChange={(value) => {
               if (value === undefined) setAmount(0);
-              else setAmount(value!);
-              console.log(value);
+              else {
+                if (value! > remainingBudget)
+                  setAmountError(
+                    `Limit amount of ${remainingBudget.toFixed(2)} exceeded.`
+                  );
+                else {
+                  setAmount(value!);
+                  setAmountError('');
+                }
+              }
             }}
-            max={remainingBudget}
+            error={amountError}
           />
 
           <ConfirmationTransfer>
-            <span>&#x20B1;{amount}</span> will be transferred to{' '}
-            <span id='destination-budget'>{label} budget</span>.
+            <span>&#x20B1;{amountError !== '' ? '0' : amount}</span> will be
+            transferred to <span id='destination-budget'>{label} budget</span>.
           </ConfirmationTransfer>
 
           <Button name='Transfer budget' />
