956a90299a7382cf484dee45e56eb42558fd1f77
thisdotEric <siguenzajohneric@gmail.com>
2022-06-13 01:25:25 +0800

    WIP - Added charting library on dashboard page

---
+++ packages/web/src/components/Charts/LineChart/LineChart.tsx
@@ -0,0 +1,77 @@
+import React, { FC } from 'react';
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const,
+    },
+    title: {
+      display: true,
+      text: 'Salary Progression Year 2022',
+    },
+  },
+};
+
+const labels = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: 'Salary',
+      data: labels.map(() => Math.random()),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Expenses',
+      data: labels.map(() => Math.random()),
+      borderColor: 'rgb(53, 162, 235)',
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+interface LineChartProps {}
+
+const LineChart: FC<LineChartProps> = ({}: LineChartProps) => {
+  return <Line options={options} data={data} />;
+};
+
+export default LineChart;
