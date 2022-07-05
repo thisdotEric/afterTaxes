4b9639543d1cccc47ca3dc1e0faa162191d5748a
thisdotEric <siguenzajohneric@gmail.com>
2022-07-05 14:45:53 +0800

    Remove unneccessary imports

--- packages/server/src/modules/users/user.repository.ts
+++ packages/server/src/modules/users/user.repository.ts
@@ -1,16 +1,13 @@
 import KnexQueryBuilder from '@database/knex/knexDatabase';
 import { DbNames, USERS } from '@database/constants';
 import { IUser } from '@entity';
-import { BaseRepository } from '@interfaces/repositories';
 import { Service } from 'fastify-decorators';
 import { hashPassword, PersistedPassword } from '@utils/auth';
 
 export type SignupUser = IUser & { password: string };
 
 @Service()
-export default class UserRepository
-  implements BaseRepository<SignupUser, number>
-{
+export default class UserRepository {
   constructor(private readonly knex: KnexQueryBuilder) {}
 
   async add(entity: SignupUser): Promise<boolean> {
